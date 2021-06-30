2d144d07ca5b593d69bb16d545da86fc3ad7c734
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-06-30 15:10:27 -0400

    commit tree 2180319d054d6d4246d14211c8be7e8e10c5545c

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+    switch(action.type){
+        case 'ADD_FRIEND':
+            // return {...state, hometown: state.hometown, id: state.id, name: state.name}
+            return ({...state, friends: [...state.friends, action.friend]})
+        case 'REMOVE_FRIEND':
+                // find the matching friend by id, comparing friend.id to action.friend.id
+                let foundFriend = state.friends.findIndex(friend => friend.id === action.id)
+                return (
+                    {...state, friends:[...state.friends.slice(0, foundFriend), ...state.friends.slice(foundFriend + 1)]})
+        default:
+            return state;
+    }
+
 }
